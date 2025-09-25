7340f1a0c53dcc32b817f965245a580a93664df1
lisiastek <patryklata11@gmail.com>
2025-09-25 22:26:33 +0200

    retranspiled app template

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const logSystem_1 = require("./logSystem");
-logSystem_1.newConsole.log("hello!");
-// (async () => {
-//     while(true){
-//         newConsole.log("test");
-//         await new Promise((res) => setTimeout(res, 200));
-//     }
-// })();
-logSystem_1.newConsole.commands.registerCommand("test", {
-    callback: async (args) => {
-        await new Promise((res) => setTimeout(res, 600));
-        logSystem_1.newConsole.log("test");
-        return true;
-    },
-    async: true
-});
-logSystem_1.newConsole.commandInterface.registerCommand("testdrugi", {
-    isAlias: true,
-    aliasName: "test",
-    hidden: false
-});
+// it will keep the process alive even though there's no error for it to be kept alive
 (0, logSystem_1.keepProcessAlive)();
