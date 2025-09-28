a2ce50f8c52e9b5856c16f4f6b6d9b014f7096e2
lisiastek <patryklata11@gmail.com>
2025-09-28 15:30:48 +0200

    app.js and app.ts to save

---
+++ old/1.171/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const logSystem_1 = require("./logSystem");
+// it will keep the process alive even though there's no error for it to be kept alive
+(0, logSystem_1.keepProcessAlive)();
---
+++ old/1.171/app.ts
@@ -0,0 +1,5 @@
+import { keepProcessAlive, newConsole } from "./logSystem";
+
+
+// it will keep the process alive even though there's no error for it to be kept alive
+keepProcessAlive();
