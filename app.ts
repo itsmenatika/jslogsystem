8d23ccdb11f28d33579a541b0126d64e5d7c3c90
lisiastek <patryklata11@gmail.com>
2025-10-02 19:16:32 +0200

    info in app.ts

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
 /**
 
-RELASE 1.0 IS NO LONGER SUPPORTED!
+RELEASE 1.15 IS HEADING TO BE LONGER SUPPORTED
 
 USE AT YOUR OWN RISK
 
-app.ts was not orginally shipped with 1.0 version. It was created afterwards!
+app.ts was not orginally shipped with 1.15 version. It was created afterwards!
 
 logSystemMIN also was not orginally SHIPPED!
 
